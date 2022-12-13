154db8b2f8e4a95d9237ea1a013aee54327693be
OmarKayed <omar2346@stud.kea.dk>
2022-12-13 18:11:54 +0100

    Added a findMinimum method for finding the person with the lowest points in LoopMetoder

--- src/main/java/LoopMetoder.java
+++ src/main/java/LoopMetoder.java
@@ -5,4 +5,15 @@ public class LoopMetoder {
            totalPoint += person.getPoint();
        } return totalPoint;
     }
+
+    public Person findMinimum (Person [] people) {
+        Person minimum = people[0];
+
+        for (Person person : people) {
+            if (person.getPoint() < minimum.getPoint()) {
+                minimum = person;
+            }
+        } return minimum;
+    }
+
 }
