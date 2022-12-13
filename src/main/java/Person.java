c4812c451b99ecb6adc401d6b13b2433731a0e33
OmarKayed <omar2346@stud.kea.dk>
2022-12-13 12:39:13 +0100

    Made a person class, and added the different attributes and a constructor

---
+++ src/main/java/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String firstName;
+    String lastName;
+    int point;
+
+    public Person (String firstName, String lastName, int point){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.point = 0;
+    }
+}
