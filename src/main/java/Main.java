b2d5af586b9a6c95fe1fd447d553edf368f9edc5
OmarKayed <omar2346@stud.kea.dk>
2022-12-13 17:28:56 +0100

    Made an enhanced for loop and some minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,33 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Person person1 = new Person("Harry", "Potter", 100);
-        Person person2 = new Person("Ron", "Weasly", 231);
-        Person person3 = new Person("Hermione", "Granger",420);
+        Person person1 = new Person("Harry", "Potter");
+        Person person2 = new Person("Ron", "Weasly");
+        Person person3 = new Person("Hermione", "Granger");
+
+        person1.addPoints(100);
+        person2.addPoints(231);
+        person3.addPoints(420);
 
         System.out.println(person1);
         System.out.println(person2);
         System.out.println(person3);
 
-        Person [] listOfStudents ={new Person("Harry", "Potter", 100),
-                new Person("Ron", "Weasly", 231),
-                new Person("Hermione", "Granger",420)};
 
+
+        // Made an array with 3 different characters
+        Person [] listOfStudents ={new Person("Harry", "Potter"),
+                new Person("Ron", "Weasly"),
+                new Person("Hermione", "Granger")};
+
+        // Made so the array can be printed with toString for Arrays
         System.out.println(Arrays.toString(listOfStudents));
+
+        for (Person listOfStudent : listOfStudents) {
+            System.out.println(listOfStudent);
+        }
+
+
 
 
 
